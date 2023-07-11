7c5352ed60a3dcc9c65dc0a6126ca030509c7ea7
Gabriel Vargas Bravo <gabriel.vargas.bravo@gmail.com>
2023-07-11 12:32:12 -0500

    base store project - frontend

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '../shared/components/ui';
+
+function HomePage() {
+  return (
+    <main>
+      <h1>Store</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
+export default HomePage;
