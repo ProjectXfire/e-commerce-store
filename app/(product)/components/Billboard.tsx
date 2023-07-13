39d8e3787c6d7cef6b72e15bc97691c58657ce38
Gabriel Vargas Bravo <gabriel.vargas.bravo@gmail.com>
2023-07-13 17:00:32 -0500

    fix: delete error messages

--- app/(product)/components/Billboard.tsx
+++ app/(product)/components/Billboard.tsx
@@ -6,26 +6,20 @@ interface Props {
 
 function Billboard({ data }: Props): JSX.Element {
   return (
-<<<<<<< HEAD
     <section className='p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden'>
-=======
-    <div className='p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden'>
->>>>>>> c03ded7da63acb58a40358cf29e8859e0ceff94a
-      <div
-        className='rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden'
-        style={{ backgroundImage: `url(${data?.imageUrl})`, backgroundSize: 'cover' }}
-      >
-        <div className='h-full w-full flex flex-col justify-center items-center gap-y-8'>
-          <div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs'>
-            {data.label}
+      <div className='p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden'>
+        <div
+          className='rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden'
+          style={{ backgroundImage: `url(${data?.imageUrl})`, backgroundSize: 'cover' }}
+        >
+          <div className='h-full w-full flex flex-col justify-center items-center gap-y-8'>
+            <div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs'>
+              {data.label}
+            </div>
           </div>
         </div>
       </div>
-<<<<<<< HEAD
     </section>
-=======
-    </div>
->>>>>>> c03ded7da63acb58a40358cf29e8859e0ceff94a
   );
 }
 export default Billboard;
--- app/(product)/components/index.ts
+++ app/(product)/components/index.ts
@@ -1,14 +1,8 @@
 export { default as Billboard } from './Billboard';
-<<<<<<< HEAD
 export { default as Filter } from './Filter';
 export { default as Gallery } from './Gallery';
 export { default as GalleryTab } from './GalleryTab';
 export { default as Info } from './Info';
 export { default as MobileFilter } from './MobileFilter';
-=======
-export { default as Gallery } from './Gallery';
-export { default as GalleryTab } from './GalleryTab';
-export { default as Info } from './Info';
->>>>>>> c03ded7da63acb58a40358cf29e8859e0ceff94a
 export { default as ProductCard } from './ProductCard';
 export { default as Products } from './Products';
